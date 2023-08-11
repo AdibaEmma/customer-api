71f7d7197d3824ed46660caad9823230292542c4
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-11 22:49:14 +0000

    add customer post request

--- src/main/java/com/aweperi/customer/CustomerController.java
+++ src/main/java/com/aweperi/customer/CustomerController.java
@@ -1,11 +1,10 @@
 package com.aweperi.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
     private final ICustomerService customerService;
 
@@ -13,13 +12,18 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/api/v1/customers")
+    @GetMapping
     List<Customer> getMockCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @GetMapping("/api/v1/customers/{id}")
+    @GetMapping("/{id}")
     Customer  getMockCustomer(@PathVariable("id") Integer id) {
         return customerService.getCustomer(id);
+    }
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest request) {
+        customerService.addCustomer(request);
     }
 }
