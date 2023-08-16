b89a6a3e18d7a72bab0df185dab94bc603f2ec4c
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-16 15:08:37 +0000

    update Customer class

--- src/main/java/com/aweperi/customer/Customer.java
+++ src/main/java/com/aweperi/customer/Customer.java
@@ -9,15 +9,24 @@ import java.util.Objects;
 @Getter
 @Entity
 @Builder
+@Table(
+        name = "customer",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "customer_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
 public class Customer {
     @Id
     @SequenceGenerator(
             name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            sequenceName = "customer_id_seq"
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            generator = "customer_id_seq"
     )
     private Integer id;
     @Column(nullable = false)
