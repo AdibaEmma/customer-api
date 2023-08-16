29eac71616933f450d03b62377c1f8a3c9c50f83
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-16 20:09:30 +0000

    setup jdbc template

---
+++ src/main/java/com/aweperi/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,59 @@
+package com.aweperi.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+@RequiredArgsConstructor
+public class CustomerJDBCDataAccessService implements CustomerDao{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        var sql = """
+                INSERT INTO customer(name, email, age)
+                VALUES (?, ?, ?)
+                """;
+        int result = jdbcTemplate.update(
+                sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge()
+        );
+        System.out.println("jdbcTemplate.update = " + result);
+    }
+
+    @Override
+    public boolean customerEmailExists(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean customerIdExists(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void removeCustomerById(Integer id) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
