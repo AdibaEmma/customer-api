e2e7d4e00ff4ff06e55c6d2cd60865901b7a0fe9
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-17 14:03:22 +0000

    test postgres test container works

--- src/test/java/com/aweperi/TestcontainersTest.java
+++ src/test/java/com/aweperi/TestcontainersTest.java
@@ -1,4 +1,23 @@
 package com.aweperi;
 
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
 public class TestcontainersTest {
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("aweperi-dao-unit-test")
+                    .withUsername("aweperi")
+                    .withPassword("password");
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
 }
