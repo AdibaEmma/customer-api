96a0e955a4ceb050aff4338648e4c72ff18348c7
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-17 14:47:52 +0000

    update

--- src/test/java/com/aweperi/TestcontainersTest.java
+++ src/test/java/com/aweperi/TestcontainersTest.java
@@ -1,6 +1,9 @@
 package com.aweperi;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,9 +18,36 @@ public class TestcontainersTest {
                     .withDatabaseName("aweperi-dao-unit-test")
                     .withUsername("aweperi")
                     .withPassword("password");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDBMigrationWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+        System.out.println();
     }
 }
