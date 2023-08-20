14871ee48a4177e660a70f2c5ddfb1ec33a1c78b
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-20 17:06:27 +0000

    write tests for CustomerRepository

--- src/test/java/com/aweperi/customer/CustomerRepositoryTest.java
+++ src/test/java/com/aweperi/customer/CustomerRepositoryTest.java
@@ -3,33 +3,67 @@ package com.aweperi.customer;
 import com.aweperi.AbstractTestcontainers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest extends AbstractTestcontainers {
+    @Autowired
     private CustomerRepository underTest;
 
     @BeforeEach
     void setUp() {
+
     }
 
     @Test
     void existsCustomerByEmail() {
         // Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                FAKER.number().numberBetween(16, 99)
+        );
+
+        underTest.save(customer);
+
 
         // When
+        var actual = underTest.existsCustomerByEmail(email);
 
         //Then
+        assertThat(actual).isTrue();
     }
 
     @Test
     void existsCustomersById() {
         // Given
+        String email = FAKER.internet().safeEmailAddress() + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                FAKER.number().numberBetween(16, 99)
+        );
+
+        underTest.save(customer);
+
+        Long id = underTest.findAll().stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
 
         // When
+        var actual = underTest.existsCustomersById(id);
 
         //Then
+        assertThat(actual).isTrue();
     }
 }
