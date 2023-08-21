1963ffe7412766eff4dd4bd223bbe173ffe4e5f6
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-21 16:25:27 +0000

    test getCustomer method

--- src/test/java/com/aweperi/customer/CustomerServiceTest.java
+++ src/test/java/com/aweperi/customer/CustomerServiceTest.java
@@ -1,12 +1,18 @@
 package com.aweperi.customer;
 
+import com.aweperi.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -28,12 +34,39 @@ class CustomerServiceTest {
     }
 
     @Test
-    void getCustomer() {
+    void canGetCustomer() {
         // Given
+        Long id = 1L;
+        var customer = Customer.builder()
+                        .id(1L)
+                        .name("Alex")
+                        .email("alex@gmail.com")
+                        .age(22)
+                        .build();
+
+        when(customerDao.selectCustomerById(id)).thenReturn( Optional.of(customer));
 
         // When
+        Customer actual = underTest.getCustomer(id);
 
         //Then
+        assertThat(actual).isEqualTo(customer);
+    }
+
+    @Test
+    void willThrowResourceNotFoundExceptionWhenGetCustomerReturnEmptyOptional() {
+        // Given
+        Long id = 10L;
+
+        when(customerDao.selectCustomerById(id)).thenReturn( Optional.empty());
+
+        // When
+        //Then
+        assertThatThrownBy(() -> underTest.getCustomer(id))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage(
+                        "customer with id [%s] not found".formatted(id)
+                );
     }
 
     @Test
