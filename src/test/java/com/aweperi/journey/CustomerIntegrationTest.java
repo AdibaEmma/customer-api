e2f1bb97ffc3e593e5e2553e14b1c2cf220899ee
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-22 11:54:10 +0000

    setup integration test

---
+++ src/test/java/com/aweperi/journey/CustomerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.aweperi.journey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomerIntegrationTest {
+    @Autowired
+    private WebTestClient webTestClient;
+}
